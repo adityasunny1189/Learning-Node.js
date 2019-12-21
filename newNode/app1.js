5af7d7f9c781750695b503361d04311638e498cf
adityasunny1189 <adityapathak1189@gmail.com>
2019-12-21 15:55:39 +0530

    file system nodejs

---
+++ newNode/app1.js
@@ -0,0 +1,19 @@
+// const fs = require("fs");
+
+// fs.appendFileSync("text1.txt", " This is appended message");
+
+// console.log("Hello World!");
+
+// console.log("Dog");
+// const name = require("./util.js");
+// console.log(name);
+
+
+const fib = require("./util.js");
+
+const calculate = require("./math.js");
+
+var ch = '*';
+console.log(calculate(4,'-',3));
+console.log(fib(20));
+
